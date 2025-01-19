4d43fd4d68fb3e6a92444b02098f9d77e83639ca
GuilhermeDunkly <118634858+GuilhermeDunkly@users.noreply.github.com>
2025-01-19 20:49:42 -0300

    Update build.sh

--- packages/mesa-panfork/build.sh
+++ packages/mesa-panfork/build.sh
@@ -1,3 +1,2 @@
 GIT_URL=https://github.com/anonymususer04/mesa-Panfork-android
 GIT_COMMIT=bbd34b254a2f4d6d30c32ac04ce1b68fcbd27d93
-LDFLAGS="-L$PREFIX/lib -landroid-shmem. a -llog -WI, --undefined-version"
