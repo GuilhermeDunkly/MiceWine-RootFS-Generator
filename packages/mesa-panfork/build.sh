db03300e368b91ba27a7e2fb310eb3f744608fb8
GuilhermeDunkly <118634858+GuilhermeDunkly@users.noreply.github.com>
2025-01-19 20:49:26 -0300

    Update build.sh

--- packages/mesa-panfork/build.sh
+++ packages/mesa-panfork/build.sh
@@ -1,10 +1,3 @@
-PKG_VER=[gss]
-PKG_CATEGORY="OpenGLDriver"
-PKG_PRETTY_NAME="Mesa Panfork Driver"
-
-
 GIT_URL=https://github.com/anonymususer04/mesa-Panfork-android
 GIT_COMMIT=bbd34b254a2f4d6d30c32ac04ce1b68fcbd27d93
 LDFLAGS="-L$PREFIX/lib -landroid-shmem. a -llog -WI, --undefined-version"
-CFLAGS="-02"
-MESON_ARGS=" -Dgallium-drivers=panfrost,swrast -Dvulkan-drivers -Dbuildtype=release -Dllvm=disabled -Dplataforms=x11,wayland"
